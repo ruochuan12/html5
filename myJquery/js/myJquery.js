5ae12a04360f839a17a6dcb47eec3dcd2ad141b5
lxchuan12 <lxchuan12@163.com>
2016-05-21 23:23:09 +0800

    add:hover,show,hide方法

--- myJquery/js/myJquery.js
+++ myJquery/js/myJquery.js
@@ -60,3 +60,28 @@ myJquery.prototype.click = function(fn) {
 	}
 	return this;
 };
+
+//设置鼠标移入移出方法
+Base.prototype.hover = function (over, out) {
+	for (var i = 0; i < this.elements.length; i ++) {
+		this.elements[i].onmouseover = over;
+		this.elements[i].onmouseout = out;
+	}
+	return this;
+};
+
+//设置显示
+Base.prototype.show = function () {
+	for (var i = 0; i < this.elements.length; i ++) {
+		this.elements[i].style.display = 'block';
+	}
+	return this;
+}
+
+//设置隐藏
+Base.prototype.hide = function () {
+	for (var i = 0; i < this.elements.length; i ++) {
+		this.elements[i].style.display = 'none';
+	}
+	return this;
+}
