0660c6087447bd8323e6135deba7171f50e2d884
lxchuan12 <lxchuan12@163.com>
2016-11-15 23:59:05 +0800

    react

---
+++ react/app.js
@@ -0,0 +1,11 @@
+class HelloWorld extend React.Component{
+    render(){
+        return (
+            <h1>Hello World!</h1>
+        );
+    }
+}
+React.render{
+    <HelloWorld/>
+    document.getElementById('content')
+}
