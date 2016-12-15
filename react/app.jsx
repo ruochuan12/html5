b18f2a0e4770a2260daee4356657aa61d7028004
lxchuan12 <lxchuan12@163.com>
2016-12-15 23:45:26 +0800

    update

--- react/app.jsx
+++ react/app.jsx
@@ -1,11 +1,34 @@
-class HelloWorld extends React.Component{
+// class HelloWorld extends React.Component{
+let HelloWorld = React.createClass({
+    getInitialState(){
+        alert('init');
+        return {
+            opacity:1,
+            color:'red',
+            fontSize:'18px'
+        }
+    },
 	render(){
 		return(
-			<h1 className="color:red;">Hello world!</h1>
-		);
-	}
-}
+            <h1 style={this.state}>Hello world!</h1>
+        );
+	},
+    componentWillMount(){
+        alert('Will');
+    },
+    componentDidMount(){
+        alert('Did');
+        let that = this;
+        window.setTimeout(()=>{
+            that.setState({
+                opacity:0.5,
+                color:'pink',
+                fontSize:'48px'
+            });
+        },1000);
+    }
+});
 React.render(
-	<HelloWorld/>
+	<HelloWorld/>,
 	document.getElementById('content')
 );
